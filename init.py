445b416a2459412bcc52a9cf8a27c9911e82e400
Shyonokaze <vinceyuhaopan@sina.com>
2018-03-08 10:03:27 +0800

    Update init.py

--- init.py
+++ init.py
@@ -5,10 +5,12 @@ Created on Wed Mar  7 11:13:35 2018
 
 @author: pyh
 """
-import xlrd
-sheet=xlrd.open_workbook('基准站配置0305.xlsx')
+
+file_name='基准站配置0305.xlsx'
 table_head=['lb','lbxh','zddh','zdmc','jsjIP','ckzbh','jsjbh','txbh','txxh',
             'jsjpp','jsjxh','ssq','dw','zdszfwq','zdszgc','sjbfdk','nwbfdk',
             'gcdlx','sjcsgs','yys','cslx','zdjrsj','dz','kjzdmc','zdbh',
             'gcz','wdyzz','yzz','hxzd','jsjwldlyhm','jsjwydlmm','bz']
 date_table_head=['zdjrsj']
+db_name='base'
+table_name='station'
