b022a80fe2afa1ca176667ddaf3d7a39f94bee06
Shyonokaze <vinceyuhaopan@sina.com>
2018-03-08 10:01:30 +0800

    Update data_sql.py

--- data_sql.py
+++ data_sql.py
@@ -4,10 +4,7 @@
 Created on Wed Mar  7 15:09:31 2018
 
 @author: pyh
-"""
-
-
-    
+"""    
 
 from mysql_data import *
 import numpy as np
@@ -19,16 +16,17 @@ db_name='data'
 db=mysql_data('root','622825')
 db.create_DATABASE(db_name)  
 
-define_table=''
-using_table=''
+
 
 
 for file_name in os.listdir(dirpath):
     table_name=file_name[0:7]
-    
+
     fid=open(dirpath+file_name,'rt')
     line=fid.readline()
     table_head=list(line.split())[0:-1]
+    define_table=''
+    using_table=''
     for i in range(len(table_head)):
         define_table=define_table+table_head[i]+' float, '
         using_table=using_table+table_head[i]+', '
@@ -43,20 +41,18 @@ for file_name in os.listdir(dirpath):
         num+=1
         line=fid.readline()        
         
-    value=[]
+    value=''
     for i in range(len(data)):
+#    for i in range(100):
+        value=value+'('
         sing=''
         for j in range(len(data[0])):
-            sing=sing+str(data[i][j])+","
-        
-        value.append(sing[0:-1])
+            sing=sing+str(data[i][j])+","        
+        value=value+sing[0:-1]+'),'
         
     db.create_TABLE(table_name,define_table[0:-2])
     
-    for i in range(len(value)):
-        if i%500 ==0:
-            print('number:%d'%i)
-        db.insert_TABLE(table_name+'('+using_table[0:-2]+')',value[i])
+    db.insert_all_TABLE(table_name+'('+using_table[0:-2]+')',value[0:-1])
         
     print(db.show_TABLE(table_name))
 
