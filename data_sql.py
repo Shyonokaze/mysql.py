0b72a70818835dc0c8901b3112b648adba1bedce
Shyonokaze <vinceyuhaopan@sina.com>
2018-03-09 10:47:26 +0800

    Update data_sql.py

--- data_sql.py
+++ data_sql.py
@@ -12,21 +12,22 @@ import os
 
 dirpath='/home/pyh/0-work/station/'
 db_name='data'   
+table_name='Station'
     
 db=mysql_data('root','622825')
 db.create_DATABASE(db_name)  
 
-
+check=True
 
 
 for file_name in os.listdir(dirpath):
-    table_name=file_name[0:7]
+#    table_name=file_name[0:7]
 
     fid=open(dirpath+file_name,'rt')
     line=fid.readline()
     table_head=list(line.split())[0:-1]
-    define_table=''
-    using_table=''
+    define_table='base_station varchar(10) charset utf8, '
+    using_table='base_station, '
     for i in range(len(table_head)):
         define_table=define_table+table_head[i]+' float, '
         using_table=using_table+table_head[i]+', '
@@ -42,15 +43,16 @@ for file_name in os.listdir(dirpath):
         line=fid.readline()        
         
     value=''
-    for i in range(len(data)):
-#    for i in range(100):
+#    for i in range(len(data)):
+    for i in range(100):
         value=value+'('
-        sing=''
+        sing="'"+str(file_name[0:7])+"'"+', '
         for j in range(len(data[0])):
             sing=sing+str(data[i][j])+","        
         value=value+sing[0:-1]+'),'
-        
-    db.create_TABLE(table_name,define_table[0:-2])
+    if check:    
+        db.create_TABLE(table_name,define_table[0:-2])
+        check=False
     
     db.insert_all_TABLE(table_name+'('+using_table[0:-2]+')',value[0:-1])
         
